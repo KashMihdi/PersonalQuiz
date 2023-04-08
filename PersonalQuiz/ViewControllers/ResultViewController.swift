e5f32c16fe7c6cd12e1abd0991bc36c159ff646f
KashMihdi <127855730+KashMihdi@users.noreply.github.com>
2023-04-08 15:54:48 +0300

    pre-final version

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,14 +9,37 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var answerChosen: [Answer]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getResult(for: answerChosen)
+    }
+    
+    private func getResult(for answers: [Answer]) {
+        var character = answers[0].animal
+        let animals = answers.map{ $0.animal }
+        var dict: [Animal : Int] = [:]
+        
+        for animal in animals {
+            dict[animal] = (dict[animal] ?? 0) + 1
+            if dict[animal] == dict.values.max() {
+                character = animal
+            }
+        }
+        
+        titleLabel.text = "Вы - \(character.rawValue)"
+        descriptionLabel.text = character.definition
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+    
     
     deinit {
         print("\(type(of: self)) has been deallocated")
